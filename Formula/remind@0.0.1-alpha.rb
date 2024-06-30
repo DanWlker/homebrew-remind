f07d66d2ba3ad87a813ee1c675cf08c1528ff967
DanWlker <danielhee2@gmail.com>
2024-07-01 00:01:04 +0800

    feat: add 0.0.1-alpha rb file

---
+++ Formula/remind@0.0.1-alpha.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class RemindAT001Alpha < Formula
+  desc "Remind is a project aware todo app that will show relevant todos depending on the project folder. It also stores all notes in $HOME/remind/ to allow for easy syncing"
+  homepage "https://github.com/DanWlker/remind/"
+  url "https://github.com/DanWlker/remind/releases/download/v0.0.1-alpha/remind"
+  sha256 "be276caa7d0d4f0db564a5854d72aebd0e0bbaa83b7f0c3ac0aca304b8303561"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build"
+    bin.install 'remind'
+  end
+
+  test do
+    system "false"
+  end
+end
